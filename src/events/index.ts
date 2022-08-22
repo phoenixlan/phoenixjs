c9aabe709fa02ec0b56747d86d20c818906afb48
Liam S. Crouch <liam@phoenixlan.no>
2022-08-22 19:05:38 +0200

    feat: add age limit field to event

--- src/events/index.ts
+++ src/events/index.ts
@@ -6,6 +6,7 @@ import { TicketType } from "../ticketType";
 
 export interface Event {
     name: string;
+    age_limit_inclusive: number;
     booking_time: number;
     cancellation_reason: null | string;
     end_time: number;
