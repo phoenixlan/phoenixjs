0c3b70eb07fd214de6be475634a8acbdd688568e
Liam S. Crouch <liam@phoenixlan.no>
2024-09-19 12:33:44 +0900

    feat(statistics): add statistics function

--- src/statistics/index.ts
+++ src/statistics/index.ts
@@ -14,6 +14,18 @@ interface TicketSaleStats {
     days: TicketSaleDay[];
 }
 
+interface UserbaseStats {
+    event: Event;
+    counts: number[];
+    crew_counts: number[];
+}
+
+interface AgeDistributionStats {
+    event: Event;
+    counts: number[];
+    crew_counts: number[];
+}
+
 export const getTicketSaleData = async (include_free: boolean): Promise<TicketSaleStats[]> => {
     const response = await fetch(`${getApiServer()}/statistics/ticket_sales${include_free?"?include_free":""}`, {
         method: 'GET',
@@ -34,4 +46,49 @@ export const getTicketSaleData = async (include_free: boolean): Promise<TicketSa
         throw new ApiGetError(error);
     }
     return await response.json() as TicketSaleStats[]
+}
+
+
+export const getUserbaseStatistics = async (): Promise<UserbaseStats[]> => {
+    const response = await fetch(`${getApiServer()}/statistics/participant_history`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            ...(await Oauth.getAuthHeaders()),
+        }
+    });
+
+    if (!response.ok) {
+        let error = ""
+        try {
+            error = (await response.json())['error']
+        } catch (e) {
+            throw new ApiGetError('Unable to get userbase stats');
+        }
+
+        throw new ApiGetError(error);
+    }
+    return await response.json() as UserbaseStats[]
+}
+
+export const getAgeDistributionStats = async (): Promise<AgeDistributionStats[]> => {
+    const response = await fetch(`${getApiServer()}/statistics/age_distribution`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            ...(await Oauth.getAuthHeaders()),
+        }
+    });
+
+    if (!response.ok) {
+        let error = ""
+        try {
+            error = (await response.json())['error']
+        } catch (e) {
+            throw new ApiGetError('Unable to get age distribution stats');
+        }
+
+        throw new ApiGetError(error);
+    }
+    return await response.json() as AgeDistributionStats[]
 }
