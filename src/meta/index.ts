e2a64f53720bac375efa1eb7737cda7fd1f01662
Liam S. Crouch <liam@phoenixlan.no>
2026-02-27 00:37:34 +0900

    New version

--- src/meta/index.ts
+++ src/meta/index.ts
@@ -1,1 +1,28 @@
 export * from './api'
+
+import {getApiServer} from "../meta";
+import {ApiGetError} from "../errors";
+
+export interface SiteConfig {
+    name: string;
+    logo: string;
+    contact: string;
+    features: string[];
+}
+
+
+export const getSiteConfig = async (): Promise<SiteConfig> => {
+    const response = await fetch(`${getApiServer()}/config`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new ApiGetError('Unable to get current site config');
+    }
+
+    return (await response.json()) as SiteConfig;
+};
+
